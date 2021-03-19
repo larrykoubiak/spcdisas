c96f440e140753a5536bdf2b7fbc008eb3f9512f
Denis Marion <koubiak@skynet.be>
2021-03-19 16:45:12 +0100

    added Gooey interface and command line option

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from gooey import Gooey, GooeyParser
 
 from spcfile import SPCFile
 from spc700 import SPC700
 
-def main(spcpath, outputpath):
-    spcfile = SPCFile(spcpath)
+@Gooey(program_name="SPC Disassembler", default_size=(800, 600))
+def main():
+    parser = GooeyParser()
+    parser.add_argument('spcfile',help="SPC File",widget="FileChooser",default="test/01 Fear of the Heavens.spc")
+    parser.add_argument('-pc', help="set starting offset for disassembly to x", default="0200")
+    parser.add_argument('-stop', help="stop disassembly at x or end of file", default="eof")
+    parser.add_argument('-a','--addr',help="enable/disable addresses", action="store_false")
+    parser.add_argument('-x', '--hex',help="enable/disable opcode hex display", action="store_false")
+    parser.add_argument('-r','--rel',help="enable/disable relative branch resolves", action="store_false")
+    parser.add_argument('outputfile',help="Target Source File",widget="FileSaver",default="test/01 Fear of the Heavens.s")
+    args = parser.parse_args()
+    print(args)
+    spcfile = SPCFile(args.spcfile)
     print(spcfile)
     spc700 = SPC700()
-    spc700.disassemble(spcfile,outputpath)
+    spc700.disassemble(spcfile, args.outputfile,args.pc,args.stop, not args.rel, not args.hex, not args.addr)
 
 if __name__ == '__main__':
-    main(r"test/01 Fear of the Heavens.spc", "test/01 Fear of the Heavens.s")
+    main()
--- spc700.py
+++ spc700.py
@@ -14,24 +14,28 @@ class SPC700:
         self.PSW = 0x00
         self.RAM = bytearray(0x10000)
 
-    def disassemble(self, spcfile: SPCFile, targetfile):
-        self.PC = 0X0200 # spcfile.pc
+    def disassemble(self, spcfile: SPCFile, targetfile, pc, stop, rel, hex, addr):
+        self.PC = int(pc, 16)
         self.A = spcfile.a
         self.X = spcfile.x
         self.Y = spcfile.y
         self.SP = spcfile.sp
         self.PSW = spcfile.psw
         self.RAM = spcfile.spcram
+        self.resolve_relative = rel
+        self.display_hex_vals = hex
+        self.display_addr = addr
         with open(targetfile, "w") as o:
-            while self.PC < 0x10000:
+            while self.PC < (0x10000 if stop=="eof" else int(stop,16)):
                 o.write(self.decodePC() + "\n")
 
     def decodePC(self):
         result = ""
         opcode = self.RAM[self.PC]
         instruction = self.__instructions[opcode]
+        formatstr = instruction["format"]
         if instruction["bytes"] == 1:
-            result = instruction["format"]
+            result = formatstr
         else:
             operands = unpack("<" + instruction["unpack"],self.RAM[self.PC+1: self.PC+instruction["bytes"]])
             formatargs=[]
@@ -40,12 +44,22 @@ class SPC700:
                 if arg in ("b","w"):
                     formatargs.append(operands[argidx])
                 elif arg=="r":
-                    formatargs.append(self.PC + operands[argidx] + instruction["bytes"])
+                    if self.resolve_relative:
+                        formatargs.append(self.PC + operands[argidx] + instruction["bytes"])
+                    else:
+                        formatstr = formatstr.replace(":04x", ":02x")
+                        formatargs.append(operands[argidx] & 0xff)
                 elif arg=="m":
                     formatargs.append(operands[argidx] & 0x1FFF)
                     formatargs.append(operands[argidx] >> 13)
                 argidx +=1
-            result = instruction["format"].format(*formatargs)
-        result = "{:04X}: ".format(self.PC) + result 
+            result = formatstr.format(*formatargs)
+        if self.display_hex_vals:
+            hexstr = ""
+            for i in range(instruction["bytes"]):
+                hexstr += "{:02x} ".format(self.RAM[self.PC+i])
+            result = hexstr + (' ' * (10-(instruction["bytes"] * 3))) + result
+        if self.display_addr:
+            result = "{:04x}: ".format(self.PC) + result 
         self.PC += instruction["bytes"]
         return result
